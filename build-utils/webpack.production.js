7a82d2902676e8cc1fd99c7e73be138330872d7a
Ivan Stanevich <i.stanevich@itransition.com>
2020-02-16 21:02:45 +0300

    webpack: split configs

---
+++ build-utils/webpack.production.js
@@ -0,0 +1,5 @@
+module.exports = ({mode}) => ({
+    output: {
+        filename: "[hash].js"
+    }
+});
--- webpack.config.js
+++ webpack.config.js
@@ -1,13 +1,20 @@
 const webpack = require('webpack');
+const webpackMerge = require('webpack-merge');
 const HtmlWebpackPlugin = require("html-webpack-plugin");
 
-module.exports = ({mode}) => ({
-    mode,
-    output: {
-        filename: "bundle.js"
-    },
-    plugins: [
-        new HtmlWebpackPlugin(),
-        new webpack.ProgressPlugin()
-    ]
-});
+const modeConfig = env => require(`./build-utils/webpack.${env}`)(env);
+
+module.exports = ({mode, presets} = {mode: 'production', presets: []}) => (
+    webpackMerge({
+            mode,
+            output: {
+                filename: "bundle.js"
+            },
+            plugins: [
+                new HtmlWebpackPlugin(),
+                new webpack.ProgressPlugin()
+            ]
+        },
+        modeConfig(mode)
+    )
+);
