6b86ace4617e79c386fc4f33890c49bd2d721b41
Ivan Stanevich <i.stanevich@itransition.com>
2020-04-26 20:51:53 +0300

    webpack: add babel with typescript, react presets

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,6 @@
 const webpack = require('webpack');
 const webpackMerge = require('webpack-merge');
+const path = require('path');
 const HtmlWebpackPlugin = require("html-webpack-plugin");
 
 const modeConfig = env => require(`./build-utils/webpack.${env}`)(env);
@@ -8,11 +9,19 @@ const presetsConfig = require('./build-utils/loadPresets');
 module.exports = ({mode, presets} = {mode: 'production', presets: []}) => (
     webpackMerge({
             mode,
+            entry: path.join(__dirname, 'src/index.tsx'),
             output: {
                 filename: "bundle.js"
             },
             module: {
                 rules: [
+                    {
+                        test: /\.(js|ts?x)$/i,
+                        exclude: /node_modules/,
+                        use: {
+                            loader: 'babel-loader'
+                        }
+                    },
                     {
                         test: /\.(png|jpe?g|gif)$/i,
                         use: [{
@@ -25,7 +34,9 @@ module.exports = ({mode, presets} = {mode: 'production', presets: []}) => (
                 ],
             },
             plugins: [
-                new HtmlWebpackPlugin(),
+                new HtmlWebpackPlugin({
+                    template: path.join(__dirname, 'src/index.html')
+                }),
                 new webpack.ProgressPlugin()
             ]
         },
