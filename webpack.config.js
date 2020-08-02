9483fee4b7027f12a3d7b0072c0de1005ac9ddb2
Ivan Stanevich <vanya.stanevich@gmail.com>
2020-08-02 18:23:48 +0300

    feature: add dot env

--- webpack.config.js
+++ webpack.config.js
@@ -2,6 +2,7 @@ const webpack = require('webpack');
 const webpackMerge = require('webpack-merge');
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
+require('dotenv').config();
 
 const modeConfig = (env) => require(`./build-utils/webpack.${env}`)(env);
 const presetsConfig = require('./build-utils/loadPresets');
@@ -40,6 +41,11 @@ module.exports = ({ mode, presets } = { mode: 'production', presets: [] }) =>
         new HtmlWebpackPlugin({
           template: path.join(__dirname, 'src/index.html'),
         }),
+        // new webpack.DefinePlugin({
+        //   ENV_VARIABLE: JSON.stringify(
+        //     process.env.ENV_VARIABLE,
+        //   ),
+        // }),
         new webpack.ProgressPlugin(),
       ],
     },
